bee3f98c167e797d8e860b604fcb6e4f13faa9fd
Lucasbk2 <lucaslopes@ucl.br>
2021-11-01 17:54:35 -0300

    Adicionando e configurando arquivos estaticos

--- covidinter/covidinter/settings.py
+++ covidinter/covidinter/settings.py
@@ -117,8 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
 
+STATIC_ROOT = 'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mainapp/static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- covidinter/mainapp/urls.py
+++ covidinter/mainapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',MainPage.as_view(),name='MainPage')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
