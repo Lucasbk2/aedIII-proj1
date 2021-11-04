d52b52b2f5b54c6e3590c20766c5c8091214ece3
Lucasbk2 <lucaslopes@ucl.br>
2021-11-04 00:58:23 -0300

    Ajuste de configurações

--- covidinter/covidinter/settings.py
+++ covidinter/covidinter/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n73&x7q!49%)$_r=kau-zzkns$br56t5a!&9nympd$twondn5h'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
