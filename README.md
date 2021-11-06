74cddb5b6aa3da6c944c245a7033fc3c263f1bdf
Lucasbk2 <lucaslopes@ucl.br>
2021-11-06 03:02:20 -0300

    Calculo de tempo e organização/estética

--- README.md
+++ README.md
@@ -5,3 +5,47 @@ Objetivo do projeto é implementar 2 algoritmos(Busca/ordenação) em cima de um
 Será utilizada neste projeto a base de dados fornecida pelo governo do canadá relacionado ao covid em escala global.
 
 https://health-infobase.canada.ca/covid-19/international/
+#Execução
+python >= 3.7 and python <= 3.9
+
+git clone https://github.com/Lucasbk2/aedIII-proj1.git
+
+criar venv/ativar(python -m pip venv venv)
+
+executar os seguintes comandos no diretorio acessado em 'covidinter/':
+
+python manage.py migrate
+
+python manage.py collectstatic
+
+python manage.py runserver
+
+##Projeto
+
+Ao selecionar o tipo de ordenação e "Realizar Ordenação", o sistema irá organizar o vetor para a realização da busca, além disso, ele ira fazer 10 ordenações/buscas para determinar a velocidade do algortimo como na figura 2.
+
+![Figura 1](./covidinter/mainapp/static/mainapp/images/c1.png)
+Figura 1
+![Figura 2](./covidinter/mainapp/static/mainapp/images/c2.png)
+Figura 2
+
+Melhor Caso: Melhor tempo de execução.
+
+Médio Caso: Tempo de execução médio.
+
+Pior Caso: Pior Tempo de execução.
+
+Ao preencher o campo da busca e realiza-lá, o sistema irá realizar a busca do valor selecionado em cima dos dados que foram de ordenação selecionados.
+
+Irá Retornar as colunas do arquivo csv junto com os dados ordenados por filtragem.
+
+![Figura 3](./covidinter/mainapp/static/mainapp/images/c3.png)
+
+#Algortimo
+Complexidade Big O(Merge Sort + Busca Linear) = O(n log n + n)
+
+Algoritmo de ordenação(Merge Sort): 'covidinter/mainapp/views.py/realiza_ordenacao/mergesort'
+
+Algoritmo de busca(Busca Linear): 'covidinter/mainapp/views.py/realiza_busca'
+
+Algoritmo de cálculo de tempo: 'covidinter/mainapp/views.py/realiza_ordenacao/geraTemposExecucao'
--- covidinter/mainapp/views.py
+++ covidinter/mainapp/views.py
@@ -1,26 +1,51 @@
+from random import randint
+
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.staticfiles.storage import staticfiles_storage
 import csv
+import datetime
 from django.http import HttpResponse
 
 
 # Create your views here.
+def pega_nome_colunas_csv():
+    path = staticfiles_storage.path('mainapp/InternationalCovid19Cases.csv')
+    arquivo = open(path)
+    csvreader = csv.reader(arquivo)
+
+    header = next(csvreader)
+    arquivo.close()
+    return header
+
 
 class MainPage(View):
     # Limita quais campos podem ser buscado no formulario
     tipos_ordenacao = ['Novos Casos', 'Total de Casos', 'Novas Mortes', 'Total de Mortes']
+    ordenacao_selecionada = None
+    colunas = pega_nome_colunas_csv()
 
     def post(self, request):
-        context = {'tipos_ordenacao': self.tipos_ordenacao}
+        context = {'tipos_ordenacao': self.tipos_ordenacao, 'ordenacao_selecionada': self.ordenacao_selecionada}
         for envio in request.POST:
             if envio == 'ordenar':
-                lista, valor = realiza_ordenacao(request.POST['ordenar'])
+                lista, valor, tempos = realiza_ordenacao(request.POST['ordenar'])
+                self.ordenacao_selecionada = request.POST['ordenar']
                 request.session['lista'] = lista
                 request.session['valor'] = valor
+                request.session['tempos'] = tempos
+                context = {'tipos_ordenacao': self.tipos_ordenacao, 'tempos': tempos,
+                           'ordenacao_selecionada': self.ordenacao_selecionada, 'colunas': self.colunas}
             elif envio == 'buscar':
-                dados_busca = realiza_busca(request.session['lista'], request.session['valor'],request.POST['buscar'])
-                context = {'tipos_ordenacao': self.tipos_ordenacao,'dados_busca': dados_busca}
+                tempos = request.session['tempos']
+                dados_busca = realiza_busca(request.session['lista'], request.session['valor'], request.POST['buscar'])
+
+                if tempos:
+                    context = {'tipos_ordenacao': self.tipos_ordenacao, 'dados_busca': dados_busca, 'tempos': tempos,
+                               'ordenacao_selecionada': self.ordenacao_selecionada, 'colunas': self.colunas,'busca':request.POST['buscar']}
+                else:
+                    context = {'tipos_ordenacao': self.tipos_ordenacao, 'dados_busca': dados_busca,
+                               'ordenacao_selecionada': self.ordenacao_selecionada, 'colunas': self.colunas,'busca':request.POST['buscar']}
         return render(request, 'mainapp/page.html', context)
 
     def get(self, request):
@@ -30,16 +55,49 @@ class MainPage(View):
 
 
 # Utilizado busca liner devido a possibilidade de elementos repetidos
-def realiza_busca(listas, index, busca):
+def realiza_busca(listas, index, busca=None):
     resultados = []
-    for x in range(len(listas)):
-        if float(listas[x][index]) == int(busca):
-            resultados.append(listas[x])
+    if busca:  # Se encontrar chave para ser buscado encontra
+        for x in range(len(listas)):
+            if float(listas[x][index]) == int(busca):
+                resultados.append(listas[x])
+    else:  # Caso não encontre, significa buscar um valor aleatório para fazer o teste de tempo em geraTemposExecucao
+        aleatorio = randint(0, len(listas))
+        for x in range(len(listas)):
+            if float(listas[x][index]) == float(listas[aleatorio][index]):
+                resultados.append(listas[x])
 
     return resultados
 
 
+# Faz a 'mesclagem' das arrays esqueda/direita as ordenando
+def mergeArray(esq, dir, indexDados):
+    final = []
+    aux, aux2 = 0, 0
+    # Realizar comparação dos valores e determina ordem que será adicionado no final
+    while aux < len(esq) and aux2 < len(dir):
+        if esq[aux][indexDados] <= dir[aux2][indexDados]:
+            final.append(esq[aux])
+            aux += 1
+        else:
+            final.append(dir[aux2])
+            aux2 += 1
 
+    final += esq[aux:]
+    final += dir[aux2:]
+    return final
+
+
+def mergesort(vetor, indexDados):
+    if (len(vetor) <= 1):  # ordenado
+        return vetor
+    centro = int(len(vetor) / 2)
+    # separação esqueda
+    esq = mergesort(vetor[:centro], indexDados)
+    # separação direita
+    dir = mergesort(vetor[centro:], indexDados)
+
+    return mergeArray(esq, dir, indexDados)
 
 
 # Ordenação por Merge Sort
@@ -47,40 +105,58 @@ def realiza_busca(listas, index, busca):
 def realiza_ordenacao(tipo):
     linhas, index = extrair_dados_csv(tipo)
 
-    # Faz a 'mesclagem' das arrays esqueda/direita as ordenando
-    def mergeArray(esq, dir, indexDados):
-        final = []
-        aux, aux2 = 0, 0
-        # Realizar comparação dos valores e determina ordem que será adicionado no final
-        while aux < len(esq) and aux2 < len(dir):
-            if esq[aux][indexDados] <= dir[aux2][indexDados]:
-                final.append(esq[aux])
-                aux += 1
-            else:
-                final.append(dir[aux2])
-                aux2 += 1
-
-        final += esq[aux:]
-        final += dir[aux2:]
-        return final
-
-    def mergesort(vetor, indexDados):
-        if (len(vetor) <= 1):  # ordenado
-            return vetor
-        centro = int(len(vetor) / 2)
-        # separação esqueda
-        esq = mergesort(vetor[:centro], indexDados)
-        # separação direita
-        dir = mergesort(vetor[centro:], indexDados)
-
-        return mergeArray(esq, dir, indexDados)
-
     # Teste em minimundo
     # linhasteste = [[1, 8], [2, 4], [3, 1], [4, 5], [5, 2], [6, 7], [7, 9], [8, 3], ]
-    # linhas = mergesort(linhasteste,1)
     linhas = mergesort(linhas, index)
+    tempos = geraTemposExecucao(linhas, index)
 
-    return linhas, index
+    return linhas, index, tempos
+
+
+def geraTemposExecucao(linhas, index):
+    # linhasteste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # centro = len(linhasteste) / 2
+    # pf = linhasteste[:int(centro)]
+    # sf = linhasteste[int(centro):]
+    # print(sf + pf)
+
+    melhor_tempo = []
+    pior_tempo = []
+    medio_tempo = []
+    # Execução medio caso
+    for x in range(4):
+        start = datetime.datetime.now()
+        linhasMedio = mergesort(linhas, index)
+        realiza_busca(linhasMedio, index)
+        fim = datetime.datetime.now()
+        medio_tempo.append(fim - start)
+
+    # Melhor caso
+    for x in range(3):
+        start = datetime.datetime.now()
+        linhasMelhor = mergesort(linhasMedio, index)
+        realiza_busca(linhasMelhor, index)
+        fim = datetime.datetime.now()
+        melhor_tempo.append(str(fim - start))
+
+    # Gera lista para execução do pior caso
+    centro = len(melhor_tempo) / 2
+    primeiraFatia = linhasMelhor[:int(centro)]
+    segundaFatia = linhasMelhor[int(centro):]
+    linhasParaPior = segundaFatia + primeiraFatia
+    # Pior Caso
+    for x in range(3):
+        start = datetime.datetime.now()
+        linhasPior = mergesort(linhasParaPior, index)
+        realiza_busca(linhasPior, index)
+        fim = datetime.datetime.now()
+        pior_tempo.append(str(fim - start))
+
+    soma = medio_tempo[0] + medio_tempo[1] + medio_tempo[2] + medio_tempo[3]
+
+    media = soma / len(medio_tempo)
+    return ["Melhor Caso: %s" % str(min(melhor_tempo)), "Médio Caso: %s" % str(media),
+            "Pior Caso: %s" % str(max(pior_tempo))]
 
 
 def extrair_dados_csv(tipo):
